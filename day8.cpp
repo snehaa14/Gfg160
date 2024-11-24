da852e8937d7321b5fc6d6811d1a42fa764c91c1
snehaa14 <asneha1412@gmail.com>
2024-11-25 00:04:40 +0530

    day8

---
+++ day8.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        int n=arr.size();
+        long long maxi = INT_MIN;
+    for(int i = 0; i<n; i++)
+    {
+        long long sum = 1;
+        for(int j = i; j<n; j++)
+        {
+           sum *= arr[j];
+           maxi = max(sum,maxi);
+           if(sum == 0) break;
+        }
+ 
+    }
+    return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
