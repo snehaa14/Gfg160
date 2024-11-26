d2fb4cd9164324fb64390927842933cacbd84d48
snehaa14 <asneha1412@gmail.com>
2024-11-27 00:10:00 +0530

    day10

---
+++ day10.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the smallest positive number missing from the array.
+    int missingNumber(vector<int> &arr) {
+        unordered_set<int> set;
+        for (int i : arr)set.insert(i);
+        int i;
+        for (i = 1; i <= set.size(); i++)
+        {
+            if (set.find(i) == set.end())return i;
+           
+        }
+       return i;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// int missingNumber(int arr[], int n);
+
+int main() {
+
+    // taking testcases
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int result = ob.missingNumber(arr);
+        cout << result << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
